b35a7aa8c8f93cc14580ac488fc2e6ef4d21e3eb
Priya <Priyamadiwalar0007@gmail.com>
2022-03-08 23:45:35 +0530

    Added:Pop method to delete last element in the linked list

--- src/com/blz/linkedlist/CounterNode.java
+++ src/com/blz/linkedlist/CounterNode.java
@@ -151,7 +151,7 @@ public class CounterNode {
 		System.out.println("\nCreated Linked list is: ");
 		sList.display();
 
-		sList.pop(0);
+		sList.pop(2);
 		System.out.println("\nLinked List after Deletion : ");
 		sList.display();
 	}
